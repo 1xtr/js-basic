74e7c56251ad2f7a3b4dc1805d81dc6a10475768
1XTR.homepc <1XTR.homepc@trendco.space>
2020-02-16 00:22:25 +0300

    Changes

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let tempC = +prompt('Insert temperature in Celsius');
 let tempF = (9/5) * tempC + 32;
-alert(tempF);
+alert(`Temperature ${tempC}C in Fahrenheit ${tempF}F `);
 
 let admin, name;
 
 name = 'Vasili';
 admin = name;
 alert(admin);
+
+let a = 1000;
+let b = '108';
+
+let summ = a + b;
+console.log(summ);
+console.log(typeof summ);
