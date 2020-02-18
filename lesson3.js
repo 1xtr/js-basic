5ebb02b9dcf9e6e82100ff47c67a9c55b970b772
1XTR.homepc <1XTR.homepc@trendco.space>
2020-02-19 00:45:41 +0300

    Lesson 3

--- lesson3.js
+++ lesson3.js
@@ -1,14 +1,48 @@
-let result = "";
-function simpleNumber(min, max) {
-    nextStep:
-    for (let  i = min; i <= max; i++) {
-        for (let j = 2; j < i; j++) {
-            if (i % j === 0) continue nextStep;
-        }
-        result += `${i},`;
-        console.log(result);
-    }
+// // Lesson 3.1
+// let result = "";
+// function simpleNumber(min, max) {
+//     let i = min;
+//     nextStep:
+//     while (i <= max) {
+//         let j = 2;
+//         while (j < i) {
+//             if (i % j === 0) continue nextStep;
+//             j++;
+//
+//         }
+//         result += `${i},`;
+//         i++;
+//     }
+//     // for (let  i = min; i <= max; i++) {
+//     //     let j = 2;
+//     //     while (i % j === 0) {
+//     //         if (j < i)
+//     //     }
+//     //     for (let j = 2; j < i; j++) {
+//     //         if (i % j === 0) continue nextStep;
+//     //     }
+//     //     result += `${i},`;
+//     //     console.log(result);
+//     // }
+//
+// }
+//
+// console.log(simpleNumber(2, 10));
 
-}
 
-console.log(simpleNumber(2, 100));
+// Lesson 3.2
+
+// Lesson 3.3
+
+// Lesson 3.4
+
+//for (let i = 0; i <= 9;) console.log(i++);
+
+// Lesson 3.5
+//  let i = x = "";
+// for (let i = 0; i <= 20; i++) console.log(x += "x");
+// or
+// while (i <= 20) {
+//     console.log(x += "x");
+//     i++;
+// }
