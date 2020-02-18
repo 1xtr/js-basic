e63b1466265bf0d2307ce2afbf811e2bc52fc457
1XTR.homepc <1XTR.homepc@trendco.space>
2020-02-18 22:24:44 +0300

    Lesson 3

---
+++ lesson3.js
@@ -0,0 +1,14 @@
+let result = "";
+function simpleNumber(min, max) {
+    nextStep:
+    for (let  i = min; i <= max; i++) {
+        for (j = 2; j < i; j++) {
+            if (i % j === 0) continue nextStep;
+        }
+        result += `${i},`;
+        console.log(result);
+    }
+
+}
+
+console.log(simpleNumber(2, 100));
