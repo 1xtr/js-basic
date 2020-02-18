361c16da60b768af37236984b0ee34f59900c300
1XTR.homepc <1XTR.homepc@trendco.space>
2020-02-18 22:25:12 +0300

    Lesson 3

--- lesson3.js
+++ lesson3.js
@@ -2,7 +2,7 @@ let result = "";
 function simpleNumber(min, max) {
     nextStep:
     for (let  i = min; i <= max; i++) {
-        for (j = 2; j < i; j++) {
+        for (let j = 2; j < i; j++) {
             if (i % j === 0) continue nextStep;
         }
         result += `${i},`;
