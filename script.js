9563e3e817b46a76f190c7ccac8486cac8e1cc87
1XTR.homepc <1XTR.homepc@trendco.space>
2020-03-02 01:47:06 +0300

    Lesson 4

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-let tempC = +prompt('Insert temperature in Celsius');
-let tempF = (9/5) * tempC + 32;
-alert(tempF);
+// function getRandomRange(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
-let admin, name;
+function getNumber() {
+    let number = +prompt("Введите целое число от 0 до 999");
+    //console.log(number + " " + typeof number);
+    if (isNaN(number)) {
+        console.log("Nan");
+        return getNumber();
+    } else if (!(number === parseInt(number, 10))) {
+        console.log("float number");
+        return getNumber();
+    } else if (number < 0 || number > 999) {
+        console.log("Not in range [0 - 999], sorry");
+        return getNumber();
+    } else {
+        console.log("gotcha!");
+        return number;
+    }
 
-name = 'Vasili';
-admin = name;
-alert(admin);
+}
+
+console.log(getNumber());
