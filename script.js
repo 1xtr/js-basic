e439bfdfed3a0a98d727a11d3d9289dcd73ce79a
1XTR.homepc <1XTR.homepc@trendco.space>
2020-03-08 02:33:22 +0300

    Lesson 6 slider

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-let tempC = +prompt('Insert temperature in Celsius');
-let tempF = (9/5) * tempC + 32;
-alert(tempF);
+'use strict';
 
-let admin, name;
+window.addEventListener('load', () => {
+    const slider = new Slider();
+    slider.run();
 
-name = 'Vasili';
-admin = name;
-alert(admin);
+});
+
+class Slider {
+    constructor() {
+        this.images = [
+            'url("img/С172/Cessna-182-Skylane-T.jpg")',
+            'url("img/С172/с182-01.jpg")',
+            'url("img/С172/с182-02.jpg")'
+        ];
+        this.currentImage = 0;
+        this.container = document.querySelector('.slider');
+        this.addListener = this.container.addEventListener('click', this.slideDirection);
+        this.sliderCtrl = document.querySelector('.slider__ctrl');
+    }
+
+    slideDirection(event) {
+        if (event.target.classList.contains('slider__ctrl_left')) {
+            return 'left';
+        } else {
+            return 'right';
+        }
+
+    }
+    nextImage() {
+        let nexEl = this.currentImage++;
+        let imgCount = this.images.length;
+        if  (nexEl < imgCount) {
+            return nexEl;
+        } else {
+            return this.currentImage = 0;
+        }
+    }
+
+    ChangeCurrentImage() {
+        this.container.style.backgroundImage  = this.images[this.nextImage()];
+    }
+    run() {
+        setInterval(this.ChangeCurrentImage.bind(this), 2000);
+    }
+}
+
